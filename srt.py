15108b39547b47ec3abffffc966383c9f7680078
Heesom <heesom.hs@gmail.com>
2023-02-19 07:49:59 +0900

    feat: add func to receive information from user

--- srt.py
+++ srt.py
@@ -6,32 +6,76 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.select import Select
-import time
-import pwd_token
+import time 
+import datetime
+import pwd_token # 아이디 받기
 
+# ---------------global variables--------------
 korail_id = pwd_token.get_korailid()
 korail_pwd = pwd_token.get_korailpwd()
-korail_start_station = '구미'
-korail_end_station = '서울'
-korail_hour = 10
-korail_month = 1
-korail_day = 25
+korail_start_station = '동대구'
+korail_end_station = '구미'
+korail_hour = '19'
+korail_year = '2023'
+korail_month = '2'
+korail_day = '18'
+# ---------------global variables--------------
 
-#앞에 달과 날이 10 미만이면 0 추가
-if korail_day < 10:
-    korail_day = '0'+ str(korail_day)
-else:
-    korail_day = str(korail_day)
 
-if korail_month < 10:
-    korail_month = '0' + str(korail_month)
-else:
-    korail_month = str(korail_month)
+TODAY = datetime.datetime.now()
 
-if korail_hour < 10:
-    korail_hour = '0' + str(korail_hour)
-else:
-    korail_hour = str(korail_hour)
+    #23/02/23     #23/2/23    # 2/23    
+    # # If year is not assigned, set default year by current year
+def get_date(date_with_year):
+    global korail_day
+    global korail_month
+    global korail_year
+
+
+    # Have to convert to int because of date_format
+    input_date = date_with_year.strip()
+    if input_date != '':
+        input_date = list(map(int, date_with_year.split('/')))
+    
+    # With year
+    if len(input_date) == 3:
+        
+        # Set year
+        if len(input_date[0]) == 2:
+            korail_year = '20' + str(input_date[0])
+        else:
+            korail_year = str(input_date[0])
+
+        korail_month = hour_day_month_format(input_date[1])
+        korail_day = hour_day_month_format(input_date[2])
+    
+    #With month and date
+    elif len(input_date) == 2:
+        korail_year = str(TODAY.year)
+        korail_month = hour_day_month_format(input_date[1])
+        korail_day = hour_day_month_format(input_date[2])
+
+    # Without month and date, set today date as default
+    elif input_date == '':
+        korail_year = str(TODAY.year)
+        korail_month = hour_day_month_format(TODAY.month)
+        korail_day = hour_day_month_format(TODAY.day)
+
+
+def get_hour(input_hour):
+    if input_hour == '':
+        korail_hour = hour_day_month_format(int(TODAY.hour))
+    elif input_hour != '':
+        korail_hour = hour_day_month_format(int(input_hour))
+
+
+# If month and date is smaller than 10, add 0 and convert to str
+# For hour, day, month
+def hour_day_month_format(int_date):
+    if int_date < 10:
+        return '0' + str(int_date)
+    else:
+        return str(int_date)
 
 
 
@@ -82,6 +126,9 @@ driver.implicitly_wait(5)
 # time.sleep(1)
 # driver.find_element(By.XPATH, '/html/body/div/div[2]/table/tbody/tr[2]/td[1]/div/div/table/tbody/tr[4]/td[4]').click()
 
+
+# 출발 월 입력
+Select(driver.find_element(By.ID,"s_year")).select_by_value(korail_year)
 
 # 출발 월 입력
 Select(driver.find_element(By.ID,"s_month")).select_by_value(korail_month)
